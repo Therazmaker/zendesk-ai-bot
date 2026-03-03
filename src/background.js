16d645ba6d633e44326128a8d23b8797943561b8
Therazmaker <63662119+Therazmaker@users.noreply.github.com>
2026-03-02 19:02:21 -0500

    Create src/background.js file with background service worker code

---
+++ src/background.js
@@ -0,0 +1,13 @@
+// Background service worker code
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing...');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetching:', event.request.url);
+});
